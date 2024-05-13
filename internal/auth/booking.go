fef3e6fc065b1c818c137457b24b4e9073be9ac0
khairulharu <khairulaswad719@gmail.com>
2024-05-13 20:36:41 +0800

    add edit booking

--- internal/auth/booking.go
+++ internal/auth/booking.go
@@ -20,13 +20,15 @@ func NewBooking(app *fiber.App, bookingService domain.BookingService, authMid fi
 
 	app.Post("/booking", authMid, handler.CreateBooking)
 	app.Get("/bookings", handler.GetAllBookings)
+	app.Delete("/booking/:id?", authMid, handler.DeleteBooking)
+	app.Patch("/booking/:id?", authMid, handler.EditBooking)
 }
 
 func (b *bookingAuth) CreateBooking(ctx *fiber.Ctx) error {
 	var bookingRequest dto.BookingRequest
 
 	if err := ctx.BodyParser(&bookingRequest); err != nil {
-		return ctx.Status(401).JSON("erorr in body parser cant parse ")
+		return ctx.Status(401).JSON("erorr in body parser cant parse get:", err.Error())
 	}
 
 	response := b.bokingService.AddBooking(ctx.Context(), bookingRequest)
@@ -50,10 +52,37 @@ func (b *bookingAuth) DeleteBooking(ctx *fiber.Ctx) error {
 	id, err := strconv.Atoi(idS)
 
 	if err != nil {
-		return ctx.SendStatus(fiber.StatusInternalServerError)
+		return ctx.Status(fiber.StatusInternalServerError).JSON(err.Error())
 	}
 
 	response := b.bokingService.DeleteBooking(ctx.Context(), dto.BookingRequest{ID: int64(id)})
+
+	return ctx.Status(util.GetHttpStatus(response.Status)).JSON(response)
+}
+
+func (b *bookingAuth) EditBooking(ctx *fiber.Ctx) error {
+
+	idS := ctx.Params("id")
+
+	if idS == ("") {
+		return ctx.Status(util.GetHttpStatus(dto.BADREQUEST)).JSON("message:id param null")
+	}
+
+	id, err := strconv.Atoi(idS)
+
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	var bookingRequest dto.BookingRequest
+
+	if err := ctx.BodyParser(&bookingRequest); err != nil {
+		ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	bookingRequest.CampsiteID = int64(id)
+
+	response := b.bokingService.UpdateBooking(ctx.Context(), bookingRequest)
 
 	return ctx.Status(util.GetHttpStatus(response.Status)).JSON(response)
 }
