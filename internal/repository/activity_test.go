ab7de3b48acf1529d727d44aadc7c9cbf55742f2
khairulharu <khairulaswad719@gmail.com>
2024-06-11 15:31:49 +0800

    add testing for repository layer

---
+++ internal/repository/activity_test.go
@@ -0,0 +1,7 @@
+package repository_test
+
+import "testing"
+
+func TestActivityRepository(t *testing.T) {
+
+}
---
+++ internal/repository/main_test.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"testing"
+
+	"gorm.io/gorm"
+)
+
+var dbGorm *gorm.DB
+
+func TestInitDatabaseTesting(t *testing.M) {
+}
