e7a39b37305145744bf574ae2b38dad3a0562546
khairulharu <khairulaswad719@gmail.com>
2024-07-06 21:30:23 +0800

    newAdd Case

--- internal/repository/availability_test.go
+++ internal/repository/availability_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestAvailabilityRepository(t *testing.T) {
-	avalaibilityRepository := repository.NewAvailabilityRepository(*&dbGorm)
+	avalaibilityRepository := repository.NewAvailabilityRepository(dbGorm)
 
 	testInsertCase := domain.Availability{
 		ID:                23,
@@ -21,6 +21,35 @@ func TestAvailabilityRepository(t *testing.T) {
 	t.Run("TestInsert", func(t *testing.T) {
 		if err := avalaibilityRepository.Insert(context.Background(), &testInsertCase); err != nil {
 			t.Error("errorinsertedvalue")
+		}
+	})
+
+	t.Run("TestUpdate", func(t *testing.T) {
+		testUpdateCase := domain.Availability{
+			ID:                23,
+			CampsiteID:        23,
+			Date:              time.Now(),
+			QuantityAvailable: 5,
+		}
+
+		if err := avalaibilityRepository.Update(context.Background(), &testUpdateCase); err != nil {
+			t.Error("errorWhenUpdtaingAvailability")
+		}
+	})
+
+	t.Run("TestFindByID", func(t *testing.T) {
+		availabilityResult, err := avalaibilityRepository.FindByID(context.Background(), testInsertCase.ID)
+
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		if availabilityResult == (domain.Availability{}) {
+			t.Error("not found response error ")
+		}
+
+		if availabilityResult.ID != testInsertCase.ID {
+			t.Error("error")
 		}
 	})
 }
