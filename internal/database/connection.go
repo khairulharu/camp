3d7ea1ff67b73f3844d72be82380ee0c74142c83
khairulharu <khairulaswad719@gmail.com>
2024-07-07 10:35:20 +0800

    add two different connection to mySql database

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"campsite/internal/config"
 	"campsite/internal/domain"
+	"database/sql"
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
@@ -38,6 +39,21 @@ func NewGormMySqlConnection(config *config.Config) *gorm.DB {
 		log.Fatalf("errror migrate get: %s", err.Error())
 	}
 	log.Println("Migrate Succesfuly")
+
+	return db
+}
+
+func NewMysqlConnection(config *config.Config) *sql.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DB.User, config.DB.Password, config.DB.Host, config.DB.Port, config.DB.Name)
+
+	log.Println("Starting to connect database")
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("error connect to database get: %s", err.Error())
+	}
+
+	log.Println("Database Connecting")
 
 	return db
 }
