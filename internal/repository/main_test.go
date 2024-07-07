389eacc5f6e67f9055161c6ec8d29e3b2c7094ae
khairulharu <khairulaswad719@gmail.com>
2024-07-07 11:46:55 +0800

    add test for user repository using manual query type

--- internal/repository/main_test.go
+++ internal/repository/main_test.go
@@ -3,17 +3,20 @@ package repository_test
 import (
 	"campsite/internal/config"
 	"campsite/internal/database"
+	"database/sql"
 	"testing"
 
 	"gorm.io/gorm"
 )
 
 var dbGorm *gorm.DB
+var dbRareTest *sql.DB
 
 func TestMain(m *testing.M) {
 	configTest := config.GetConfigTest()
 
 	dbGorm = database.NewGormMySqlConnection(configTest)
+	dbRareTest = database.NewMysqlConnection(configTest)
 
 	m.Run()
 }
--- internal/repository/user_test.go
+++ internal/repository/user_test.go
@@ -1,10 +1,28 @@
 package repository_test
 
 import (
+	"campsite/internal/domain"
 	"campsite/internal/repository"
+	"context"
 	"testing"
+	"time"
 )
 
 func TestUserRepositoryRare(t *testing.T) {
-	userRepositoryRare := repository.NewUserRepositoryRare()
+	userRepositoryRare := repository.NewUserRepositoryRare(dbRareTest)
+
+	testInsertCase := domain.User{
+		Name:        "kahirul",
+		Email:       "aswad@gmail.commmm",
+		Password:    "sjshdjs",
+		PhoneNumber: "0888888888888",
+		Address:     "akksjad, planet mars",
+		CreatedAt:   time.Now(),
+	}
+
+	t.Run("InsertRareUser", func(t *testing.T) {
+		if err := userRepositoryRare.Insert(context.Background(), &testInsertCase); err != nil {
+			t.Log(err)
+		}
+	})
 }
